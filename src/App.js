83ef382a2cde83421e4c676ccffe0ea403daab34
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-14 16:13:56 +0200

    added the shuffling cards logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/feather-1.png"},
+  { "src": "/img/magicstone-1.png"},
+  { "src": "/img/potion-1.png"},
+  { "src": "/img/ravenskull-1.png"},
+  { "src": "/img/tigerskull-1.png"},
+  { "src": "/img/wizardhat-1.png"}
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random }) )
+
+    setCards(shuffledCards);
+    setTurns(0);
+  }
+   
   return (
     <div className="App">
       <h1>Magic Cards Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
