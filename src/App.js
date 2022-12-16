d4701e1f6d99d54316dc56c33b5333808b1e7731
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-16 14:16:35 +0200

    add a match propery

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
 import "./components/SingleCard.js"
 import SingleCard from './components/SingleCard.js';
 
 const cardImages = [
-  { "src": "/img/feather-1.png"},
-  { "src": "/img/magicstone-1.png"},
-  { "src": "/img/potion-1.png"},
-  { "src": "/img/ravenskull-1.png"},
-  { "src": "/img/tigerskull-1.png"},
-  { "src": "/img/wizardhat-1.png"}
+  { "src": "/img/feather-1.png", matched: false},
+  { "src": "/img/magicstone-1.png", matched: false},
+  { "src": "/img/potion-1.png", matched: false},
+  { "src": "/img/ravenskull-1.png", matched: false},
+  { "src": "/img/tigerskull-1.png", matched: false},
+  { "src": "/img/wizardhat-1.png", matched: false}
 ]
 
 function App() {
+  // variables
+  const numberOfTurns = 2;
+  let isGameOver = false;
+
   // useStates
   const [cards, setCards] = useState([])
-  const [turns, setTurns] = useState(0)
+  const [turns, setTurns] = useState(numberOfTurns)
 
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
@@ -27,18 +32,49 @@ function App() {
       .map((card) => ({ ...card, id: Math.random }) )
 
     setCards(shuffledCards);
-    setTurns(0);
+    setTurns(numberOfTurns);
   }
 
   // handle card choice
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-
-    // 
   }
-   
+
+  // compare 2 selected card
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card;
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        console.log("NO match");
+        resetTurn()
+      }
+    }
+    
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards);  
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
+
   return (
     <div className="App">
+      <h2>Turns: {turns}</h2>
       <h1>Magic Cards Match</h1>
       <button onClick={shuffleCards}>New Game</button>
 
